f5d569bd4b07e2779dc3da9de29b5b1b38d792e2
surajpxl <guptasuraj4455@gmail.com>
2025-07-27 23:58:12 +0530

    some updates

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -13,9 +13,10 @@ function Course() {
       } catch (error) {
         console.log(error);
       }
-    };
+    }; 
     getBook();
   }, []);
+  
   return (
     <>
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
@@ -25,13 +26,7 @@ function Course() {
             <span className="text-pink-500"> Here! :)</span>
           </h1>
           <p className="mt-12">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
-            assumenda? Repellendus, iste corrupti? Tempore laudantium
-            repellendus accusamus accusantium sed architecto odio, nisi expedita
-            quas quidem nesciunt debitis dolore non aspernatur praesentium
-            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
-            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
-            consequatur!
+           "Our Book Store website offers a wide collection of books across various genres, making it easy for readers to discover their next favorite read. With a clean and user-friendly design, browsing and purchasing books is quick and convenient. Features like detailed book descriptions, search functionality, and category filters enhance the shopping experience. Whether you’re a casual reader or a book enthusiast, this platform brings the joy of reading right to your fingertips."
           </p>
           <Link to="/">
             <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
