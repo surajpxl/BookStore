d0abef922b0be9648b930f2f7ee9f1d873aaba5a
surajpxl <guptasuraj4455@gmail.com>
2025-09-13 18:12:56 +0530

    fix: use node instead of nodemon for start script

--- Backend/index.js
+++ Backend/index.js
@@ -6,31 +6,32 @@ import cors from "cors";
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
-const app = express();
+dotenv.config();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-dotenv.config();
-
 const PORT = process.env.PORT || 4001;
-const URI = process.env.MongoDBURI;
+const URI = process.env.MONGO_URI;
 
-// connect to mongoDB
-try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+// connect to MongoDB
+(async () => {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    console.log("Connected to mongoDB");
-} catch (error) {
-    console.log("Error: ", error);
-}
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+})();
 
-// defining routes
+// routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`🚀 Server is listening on port ${PORT}`);
 });
