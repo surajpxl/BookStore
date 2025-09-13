674cc0718078e1a2babdd9e3f3dc1282dcbc9717
surajpxl <guptasuraj4455@gmail.com>
2025-09-13 18:39:10 +0530

    ready for Vercel deploy

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,8 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await  axios.get("https://bookstore-2-dn41.onrender.com/book")
+
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
